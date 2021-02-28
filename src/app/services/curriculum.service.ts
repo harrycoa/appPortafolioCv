970c78bce150300bd77bb000a7db81358113e43b
Harry Coa <rodol_2000@hotmail.com>
2021-02-27 21:06:11 -0500

    cambios nuevos en el servicio

--- src/app/services/curriculum.service.ts
+++ src/app/services/curriculum.service.ts
@@ -13,7 +13,7 @@ export class CurriculumService {
   blog: any[] = [];
 
   constructor(private http: HttpClient) {
-    console.log('firebase');
+    //console.log('firebase');
     this.cargarProfesional();
     this.cargarEducacion();
     this.cargarTestimonio();
@@ -25,28 +25,28 @@ export class CurriculumService {
     this.http.get('https://cv-harry-default-rtdb.firebaseio.com/Profesional.json')
       .subscribe(( resp: any[]) => {
         this.profesional = resp;
-        console.log(resp);
+        //console.log(resp);
       });
   }
   private cargarEducacion(){
     this.http.get('https://cv-harry-default-rtdb.firebaseio.com/Educacion.json')
       .subscribe(( resp: any[]) => {
         this.educacion = resp;
-        console.log(resp);
+        //console.log(resp);
       });
   }
   private cargarTestimonio(){
     this.http.get('https://cv-harry-default-rtdb.firebaseio.com/Testimonio.json')
       .subscribe(( resp: any[]) => {
         this.testimonio = resp;
-        console.log(resp);
+        //console.log(resp);
       });
   }
   private cargarExperiencia(){
     this.http.get('https://cv-harry-default-rtdb.firebaseio.com/Experiencia.json')
       .subscribe(( resp: any[]) => {
         this.experiencia = resp;
-        console.log(resp);
+        //console.log(resp);
       });
   }
 
@@ -55,7 +55,7 @@ export class CurriculumService {
     this.http.get('https://cv-harry-default-rtdb.firebaseio.com/Blog.json')
       .subscribe(( resp: any[]) => {
         this.blog = resp;
-        console.log(resp);
+        //console.log(resp);
       });
   }
 
